86e56c7775ad6e7058b7fe7ad4384a5ad9330cb9
Luoie8340 <itslukiee@gmail.com>
2024-03-16 17:53:32 +0800

    webpack initial

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackLogin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackLogin({
+      title: "Webpack Setup",
+      filename: "index.html",
+      template: path.resolve(__dirname, "template.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+};
