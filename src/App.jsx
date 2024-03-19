c27e59268e685d55ff87c3d86b4db4095cec4b24
Luoie8340 <itslukiee@gmail.com>
2024-03-20 03:41:55 +0800

    react router later

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
+    <div className='bg-slate-400'>
       <h1 className='text-4xl font-bold underline bg-slate-600'>webpack-setup</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem dicta tempore alias nihil qui, reprehenderit rerum fuga distinctio nisi, praesentium molestias id dignissimos blanditiis hic tempora nulla odio, quia quaerat.</p>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import "./main.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
